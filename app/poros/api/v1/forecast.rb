461827988802d03a49eefb22fa618af134b7b22a
Joshua Carey <jdcarey128@gmail.com>
2021-01-17 09:54:36 -0700

    Parse and format forecast response in forecast poro

--- app/poros/api/v1/forecast.rb
+++ app/poros/api/v1/forecast.rb
@@ -3,12 +3,13 @@ module Api
     class Forecast 
       attr_reader :current_weather, 
                   :hourly_weather, 
-                  :daily_weather
+                  :daily_weather,
+                  :dir
                   
       def initialize(forecast_data)
         @current_weather = format_current(forecast_data[:current])
-        # @hourly_weather = format_hourly(forecast_data[:hourly])
-        # @daily_weather = format_daily(forecast_data[:daily])
+        @daily_weather = format_daily(forecast_data[:daily])
+        @hourly_weather = format_hourly(forecast_data[:hourly])
       end
 
       private 
@@ -28,9 +29,47 @@ module Api
         }
       end
 
+      def format_daily(daily_data)
+        daily_data[1..5].map do |day|
+          {
+            date: to_time(day[:dt]).strftime("%D"),
+            sunrise: to_time(day[:sunrise]),
+            sunset: to_time(day[:sunset]),
+            max_temp: day[:temp][:max],
+            min_temp: day[:temp][:min],
+            conditions: day[:weather][0][:description],
+            icon: day[:weather][0][:icon]
+          }
+        end
+      end
+
+      def format_hourly(hourly_data)
+        hourly_data[1..8].map do |hour|
+          {
+            time: to_time(hour[:dt]).strftime('%T'),
+            temperature: hour[:temp],
+            wind_speed: "#{hour[:wind_speed]} mph",
+            wind_direction: "from #{dir[wind_dir(hour[:wind_deg])]}",
+            conditions: hour[:weather][0][:description], 
+            icon: hour[:weather][0][:icon]
+          }
+        end
+      end
+
       def to_time(num)
         Time.at(num)
-        #add .to_date for just date 
+      end
+
+      def wind_dir(wind)
+        ((wind % 360) / 22.5).round(0)
+      end
+
+      def dir 
+        return [
+          'N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 
+          'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW',
+          'NW', 'NNW', 'N'
+        ]
       end
     end
   end
--- spec/poros/api/v1/forecast_spec.rb
+++ spec/poros/api/v1/forecast_spec.rb
@@ -7,8 +7,11 @@ RSpec.describe Api::V1::Forecast do
 
     result = Api::V1::Forecast.new(forecast) 
     current = result.current_weather
+    daily = result.daily_weather
+    hourly = result.hourly_weather 
     expect(result).to be_a(Api::V1::Forecast)
 
+    # Check current weather 
     expect(current).to be_a(Hash)
     expect(current[:datetime]).to be_a(Time)
     expect(current[:sunrise]).to be_a(Time)
@@ -21,7 +24,27 @@ RSpec.describe Api::V1::Forecast do
     expect(current[:conditions]).to eq('clear sky')
     expect(current[:icon]).to eq('01d')
 
-    #TODO: finish test with hourly_weather and daily_weather 
+    # Check daily weather 
+    expect(daily).to be_a(Array)
+    expect(daily.length).to eq(5)
+    first = daily[0]
+    expect(first[:date]).to eq('01/17/21')
+    expect(first[:sunrise]).to be_a(Time)
+    expect(first[:sunset]).to be_a(Time)
+    expect(first[:max_temp]).to eq(45.54)
+    expect(first[:min_temp]).to eq(32.22)
+    expect(first[:conditions]).to eq('overcast clouds')
+    expect(first[:icon]).to eq('04d')
     
+    # Check hourly weather 
+    expect(hourly).to be_a(Array)
+    expect(hourly.length).to eq(8)
+    first = hourly[0]
+    expect(first[:time]).to eq('17:00:00')
+    expect(first[:temperature]).to eq(41.18)
+    expect(first[:wind_speed]).to eq('5.12 mph')
+    expect(first[:wind_direction]).to eq('from SSE')
+    expect(first[:conditions]).to eq('clear sky')
+    expect(first[:icon]).to eq('01d')
   end
 end
