fd7f636ec0fee963ef8c806e74367b7649ea1f91
Joshua Carey <jdcarey128@gmail.com>
2021-01-17 10:03:55 -0700

    Build out test for happy forecast facade test

--- spec/facades/api/v1/forecast_facade_spec.rb
+++ spec/facades/api/v1/forecast_facade_spec.rb
@@ -12,11 +12,45 @@ RSpec.describe Api::V1::ForecastFacade do
       allow(Api::V1::ForecastService).to receive(:get_forecast).with(coords).and_return(forecast)
       
       result = Api::V1::ForecastFacade.get_forecast(location)
+      current = result.current_weather
+      daily = result.daily_weather[0]
+      hourly = result.hourly_weather[0]
 
-      expect(result).to be_a(Forecast)
-      expect(result.current_weather).to be_a(Hash)
+      expect(result).to be_a(Api::V1::Forecast)
+      expect(current).to be_a(Hash)
       expect(result.daily_weather).to be_a(Array)
       expect(result.hourly_weather).to be_a(Array)
+      expect(daily).to be_a(Hash)
+      expect(hourly).to be_a(Hash)
+
+      #Check current weather format 
+      expect(current[:datetime]).to be_a(Time)
+      expect(current[:sunrise]).to be_a(Time)
+      expect(current[:sunset]).to be_a(Time)
+      expect(current[:temperature]).to be_a(Float)
+      expect(current[:feels_like]).to be_a(Float)
+      expect(current[:humidity]).to be_a(Numeric)
+      expect(current[:uvi]).to be_a(Numeric)
+      expect(current[:visibility]).to be_a(Numeric)
+      expect(current[:conditions]).to be_a(String)
+      expect(current[:icon]).to be_a(String)
+
+      #Check daily weather format 
+      expect(daily[:date]).to be_a(String)
+      expect(daily[:sunrise]).to be_a(Time)
+      expect(daily[:sunset]).to be_a(Time)
+      expect(daily[:max_temp]).to be_a(Float)
+      expect(daily[:min_temp]).to be_a(Float)
+      expect(daily[:conditions]).to be_a(String)
+      expect(daily[:icon]).to be_a(String)
+
+      #Check hourly weather format 
+      expect(hourly[:time]).to be_a(String)
+      expect(hourly[:temperature]).to be_a(Float)
+      expect(hourly[:wind_speed]).to be_a(String)
+      expect(hourly[:wind_direction]).to be_a(String)
+      expect(hourly[:conditions]).to be_a(String)
+      expect(hourly[:icon]).to be_a(String)
     end
   end
 end
