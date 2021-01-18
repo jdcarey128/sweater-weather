45b4cb9e93bce79abc861733b3c38d14101b0e3a
Joshua Carey <jdcarey128@gmail.com>
2021-01-18 16:59:19 -0700

    Add user model and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  before_validation :generate_api_key
+  
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates_presence_of :password, require: true 
+  validates :api_key, presence: true, uniqueness: true 
+  
+  has_secure_password
+
+  def generate_api_key
+    self.api_key = SecureRandom.uuid
+  end
 end
