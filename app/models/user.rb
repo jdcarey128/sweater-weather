eaf3edb54504197b94e2dad7222d9565531630a0
Joshua Carey <jdcarey128@gmail.com>
2021-01-18 17:46:00 -0700

    Refactor user model and add user factory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
   before_validation :generate_api_key
-  
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates_presence_of :password, require: true 
-  validates :api_key, presence: true, uniqueness: true 
   
   has_secure_password
+
+  private 
 
   def generate_api_key
     self.api_key = SecureRandom.uuid
