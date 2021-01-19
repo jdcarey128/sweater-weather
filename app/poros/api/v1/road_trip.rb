1bf7a85ac4c6eeaa035edb3749e1ca2a5ff68cb3
Joshua Carey <jdcarey128@gmail.com>
2021-01-19 15:59:19 -0700

    Add start city to road trip poro

--- app/poros/api/v1/road_trip.rb
+++ app/poros/api/v1/road_trip.rb
@@ -1,18 +1,20 @@
 module Api 
   module V1 
     class RoadTrip 
-      attr_reader :destination_city, 
+      attr_reader :start_city,
+                  :destination_city, 
                   :travel_time,
                   :destination_coords
 
       def initialize(travel_info)
-        @destination_city = format_dest(travel_info[:locations][-1])
+        @start_city = format_city(travel_info[:locations][0])
+        @destination_city = format_city(travel_info[:locations][-1])
         @travel_time = travel_info[:formattedTime]
         @destination_coords = travel_info[:locations][-1][:displayLatLng]
       end
 
-      def format_dest(dest)
-        "#{dest[:adminArea5]}, #{dest[:adminArea3]}"
+      def format_city(city)
+        "#{city[:adminArea5]}, #{city[:adminArea3]}"
       end
     end
   end
--- spec/poros/api/v1/road_trip_poro_spec.rb
+++ spec/poros/api/v1/road_trip_poro_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Api::V1::RoadTrip do
     travel = Api::V1::RoadTrip.new(travel_info)
 
     expect(travel).to be_a(Api::V1::RoadTrip)
+    expect(travel.start_city).to eq('Denver, CO')
     expect(travel.destination_city).to eq('Boulder, CO')
     expect(travel.destination_coords).to eq({lng: -105.279266, lat: 40.015831})
     expect(travel.travel_time).to eq("00:35:23")
