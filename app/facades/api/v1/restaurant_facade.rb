b225959f91afb29819df924ff10b8dc5bf0d394c
Joshua Carey <jdcarey128@gmail.com>
2021-01-19 11:24:42 -0700

    Add restaurant facade with passing test

--- app/facades/api/v1/restaurant_facade.rb
+++ app/facades/api/v1/restaurant_facade.rb
@@ -2,9 +2,9 @@ module Api
   module V1 
     class RestaurantFacade 
       
-      def self.find_restaurant(food)
-        restaurant_info = RestaurantService.find_restaurant(food)
-        # Add poro 
+      def self.find_restaurant(food, coords)
+        restaurant_info = RestaurantService.find_restaurant(food, coords)
+        Restaurant.new(restaurant_info)
       end
     end
   end
--- spec/facades/api/v1/restaurant_facade_spec.rb
+++ spec/facades/api/v1/restaurant_facade_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::RestaurantFacade do 
   describe 'find restaurant with valid food params' do 
-    it 'returns restaurant object' do 
-      result = Api::V1::RestaurantFacade.find_restaurant(food)
+    it 'returns restaurant object', :vcr do 
+      food = 'cheese burger'
+      coords = {:lng=>-105.279266, :lat=>40.015831}
 
-      expect(result).to be_a(Api::V1::RestaurantFacade)
+      result = Api::V1::RestaurantFacade.find_restaurant(food, coords)
+
+      expect(result).to be_a(Api::V1::Restaurant)
     end
   end
 end
